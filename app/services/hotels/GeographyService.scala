220037b6d044c29702657ac1d7df8ab6c2e63212
Greg Dorrell <Greg@Dorrell.me>
2017-02-19 23:59:45 +0000

    Lesson 3

--- app/services/hotels/GeographyService.scala
+++ app/services/hotels/GeographyService.scala
@@ -7,15 +7,8 @@ import model.Coordinates
 @Singleton
 class GeographyService {
 
-  val destinations = Map(
-    "london" -> Coordinates(51.507351, -0.127758),
-    "paris" -> Coordinates(48.856614, 2.352222),
-    "bath" -> Coordinates(51.375801, -2.359904),
-    "birmingham" -> Coordinates(52.486243, -1.890401)
-  )
-
   def lookupDestination(name: String): Option[Coordinates] = {
-    destinations.get(name.toLowerCase.trim)
+    ???
   }
 
 }
--- app/services/hotels/HotelsService.scala
+++ app/services/hotels/HotelsService.scala
@@ -2,15 +2,15 @@ package services.hotels
 
 import javax.inject.Inject
 
+import model.Hotel
+
 class HotelsService @Inject() (
                                 catalogueService: HotelCatalogueService,
                                 geographyService: GeographyService,
                                 hotelFinderService: HotelFinderService) {
 
-  def search(destination: String, radius: Double) = for {
-    coordinates <- geographyService.lookupDestination(destination).toSeq
-    hotelId <- hotelFinderService.findHotels(coordinates, radius)
-    hotel <- catalogueService.lookupHotel(hotelId)
-  } yield hotel
+  def search(destination: String, radius: Double): Seq[Hotel] = {
+    ???
+  }
 
 }
