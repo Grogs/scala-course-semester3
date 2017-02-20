075889ee948ef2cfbe3382e4e643ac79bf8922c1
Greg Dorrell <Greg@Dorrell.me>
2017-02-20 00:34:29 +0000

    Update tests

--- test/services/hotels/GeographyServiceTest.scala
+++ test/services/hotels/GeographyServiceTest.scala
@@ -25,14 +25,14 @@ class GeographyServiceTest extends FunSuite with Matchers {
 
   test("Bath") {
     service.lookupDestination("Bath") shouldBe Some(Coordinates(51.375801, -2.359904))
-    service.lookupDestination("Bath") shouldBe Some(Coordinates(51.375801, -2.359904))
-    service.lookupDestination("Bath ") shouldBe Some(Coordinates(51.375801, -2.359904))
+    service.lookupDestination("bath") shouldBe Some(Coordinates(51.375801, -2.359904))
+    service.lookupDestination("bath ") shouldBe Some(Coordinates(51.375801, -2.359904))
   }
 
   test("Birmingham") {
     service.lookupDestination("Birmingham") shouldBe Some(Coordinates(52.486243, -1.890401))
-    service.lookupDestination("Birmingham") shouldBe Some(Coordinates(52.486243, -1.890401))
-    service.lookupDestination("birmingham ") shouldBe Some(Coordinates(52.486243, -1.890401))
+    service.lookupDestination("birmingham") shouldBe Some(Coordinates(52.486243, -1.890401))
+    service.lookupDestination("Birmingham ") shouldBe Some(Coordinates(52.486243, -1.890401))
   }
 
 }
