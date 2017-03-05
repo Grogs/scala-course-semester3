2f416cb8455ac995c7647b3b2b71d8b6fdff4fa1
Greg Dorrell <Greg@Dorrell.me>
2017-03-05 00:00:09 +0000

    Implement HotelsController and HotelsService

--- server/app/services/hotels/HotelsService.scala
+++ server/app/services/hotels/HotelsService.scala
@@ -4,12 +4,14 @@ import javax.inject.Inject
 
 import model.Hotel
 
-class HotelsService @Inject() (
-                                catalogueService: HotelCatalogueService,
-                                geographyService: GeographyService,
-                                hotelFinderService: HotelFinderService) {
+class HotelsService @Inject()(
+                               catalogueService: HotelCatalogueService,
+                               geographyService: GeographyService,
+                               hotelFinderService: HotelFinderService) {
 
-  def search(destination: String, radius: Double): Seq[Hotel] = {
-    ???
-  }
+  def search(destination: String, radius: Double): Seq[Hotel] = for {
+    coordinates <- geographyService.lookupDestination(destination).toSeq
+    hotelId <- hotelFinderService.findHotels(coordinates, radius)
+    hotel <- catalogueService.lookupHotel(hotelId)
+  } yield hotel
 }
