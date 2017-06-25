ba1d15a28bd368290b1d395a44383d004caf446c
Greg Dorrell <Greg@Dorrell.me>
2017-06-25 20:36:25 +0100

    Add more boilerplate for Autowire.

--- server/app/controllers/HotelsController.scala
+++ server/app/controllers/HotelsController.scala
@@ -2,8 +2,12 @@ package controllers
 
 import javax.inject.{Inject, Singleton}
 
-import play.api.mvc.InjectedController
+import autowire.Core.Request
+import play.api.mvc.{Action, InjectedController}
 import services.hotels.HotelsService
+import upickle.{Js, json}
+import upickle.default._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 @Singleton
 class HotelsController @Inject()(hotelsService: HotelsService, webJarAssets: WebJarAssets) extends InjectedController {
@@ -22,6 +26,29 @@ class HotelsController @Inject()(hotelsService: HotelsService, webJarAssets: Web
     } else {
       BadRequest("Invalid distance")
     }
+  }
+
+
+  object ApiServer extends autowire.Server[Js.Value, Reader, Writer] {
+    def read[Result: Reader](p: Js.Value) = upickle.default.readJs[Result](p)
+    def write[Result: Writer](r: Result) = upickle.default.writeJs(r)
+  }
+
+  def api(path: String) = Action.async{ implicit req =>
+
+    val body = req.body.asText.getOrElse("")
+
+    val parameters = json.read(body)
+      .asInstanceOf[Js.Obj]
+      .value
+      .toMap
+
+    val request = Request(path.split("/"), parameters)
+
+    for {
+      resp <- ApiServer.route[HotelsService](hotelsService)(request)
+    } yield
+      Ok(json.write(resp))
   }
 
 }
